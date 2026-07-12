3d4e29adb96248e6e5635df671c19023b9517315
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:48 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "iot_security_device_groups" {
-  description = "All iot_security_device_group resources"
-  value       = azurerm_iot_security_device_group.iot_security_device_groups
-}
 output "iot_security_device_groups_allow_rule" {
-  description = "List of allow_rule values across all iot_security_device_groups"
-  value       = [for k, v in azurerm_iot_security_device_group.iot_security_device_groups : v.allow_rule]
+  description = "Map of allow_rule values across all iot_security_device_groups, keyed the same as var.iot_security_device_groups"
+  value       = { for k, v in azurerm_iot_security_device_group.iot_security_device_groups : k => v.allow_rule }
 }
 output "iot_security_device_groups_iothub_id" {
-  description = "List of iothub_id values across all iot_security_device_groups"
-  value       = [for k, v in azurerm_iot_security_device_group.iot_security_device_groups : v.iothub_id]
+  description = "Map of iothub_id values across all iot_security_device_groups, keyed the same as var.iot_security_device_groups"
+  value       = { for k, v in azurerm_iot_security_device_group.iot_security_device_groups : k => v.iothub_id }
 }
 output "iot_security_device_groups_name" {
-  description = "List of name values across all iot_security_device_groups"
-  value       = [for k, v in azurerm_iot_security_device_group.iot_security_device_groups : v.name]
+  description = "Map of name values across all iot_security_device_groups, keyed the same as var.iot_security_device_groups"
+  value       = { for k, v in azurerm_iot_security_device_group.iot_security_device_groups : k => v.name }
 }
 output "iot_security_device_groups_range_rule" {
-  description = "List of range_rule values across all iot_security_device_groups"
-  value       = [for k, v in azurerm_iot_security_device_group.iot_security_device_groups : v.range_rule]
+  description = "Map of range_rule values across all iot_security_device_groups, keyed the same as var.iot_security_device_groups"
+  value       = { for k, v in azurerm_iot_security_device_group.iot_security_device_groups : k => v.range_rule }
 }
 
