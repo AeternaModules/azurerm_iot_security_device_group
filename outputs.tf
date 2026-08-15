4b77d62af63a13876d89c519d11e16349c01c59e
Daniel Allen <dan@aeternagroup.net>
2026-08-15 01:03:20 +0100

    Update to schema version v4.81.0

--- outputs.tf
+++ outputs.tf
@@ -4,7 +4,7 @@ output "iot_security_device_groups_id" {
 }
 output "iot_security_device_groups_allow_rule" {
   description = "Map of allow_rule values across all iot_security_device_groups, keyed the same as var.iot_security_device_groups"
-  value       = { for k, v in azurerm_iot_security_device_group.iot_security_device_groups : k => v.allow_rule if v.allow_rule != null && length(v.allow_rule) > 0 }
+  value       = { for k, v in azurerm_iot_security_device_group.iot_security_device_groups : k => one(v.allow_rule) if v.allow_rule != null && length(v.allow_rule) > 0 }
 }
 output "iot_security_device_groups_iothub_id" {
   description = "Map of iothub_id values across all iot_security_device_groups, keyed the same as var.iot_security_device_groups"
